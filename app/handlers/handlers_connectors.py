661b7414ee1ac51c623b56d16fe223fc1239224a
Eran Kampf <eran@ekampf.com>
2023-11-01 22:30:19 -0700

    handle pod deletion

--- app/handlers/handlers_connectors.py
+++ app/handlers/handlers_connectors.py
@@ -3,13 +3,19 @@ import base64
 import kopf
 import kubernetes.client
 
-from app.crds import ConnectorSpec, TwingateConnectorCRD
+from app.crds import TwingateConnectorCRD
+from app.handlers.base import success
 from app.settings import get_version
+
+ANNOTATION_LAST_VERSION_CHECK = "twingate.com/last-version-check"
+ANNOTATION_NEXT_VERSION_CHECK = "twingate.com/next-version-check"
 
 
 def get_connector_pod(
-    name: str, url: str, spec: ConnectorSpec
+    crd: TwingateConnectorCRD, tenant_url: str, tag: str
 ) -> kubernetes.client.V1Pod:
+    spec = crd.spec
+    name = crd.metadata.name
     # fmt: off
     pod_spec = {
         "containers": [
@@ -17,7 +23,7 @@ def get_connector_pod(
                 "env": [
                     {"name": "TWINGATE_LABEL_DEPLOYED_BY", "value": "operator"},
                     {"name": "TWINGATE_LABEL_OPERATOR_VERSION", "value": get_version()},
-                    {"name": "TWINGATE_URL", "value": url},
+                    {"name": "TWINGATE_URL", "value": tenant_url},
                     {"name": "TWINGATE_LOG_LEVEL", "value": "7"},
                 ],
                 "envFrom": [
@@ -28,7 +34,7 @@ def get_connector_pod(
                         }
                     }
                 ],
-                "image": "twingate/connector:1",
+                "image": f"twingate/connector:{tag}",
                 "imagePullPolicy": "Always",
                 "name": "connector",
                 "securityContext": {
@@ -60,17 +66,19 @@ def get_connector_secret(
 
 
 @kopf.on.create("twingateconnector")
-def twingate_connector_create(body, spec, memo, logger, namespace, patch, **_):
+def twingate_connector_create(body, memo, logger, namespace, patch, reason, **_):
     settings = memo.twingate_settings
     client = memo.twingate_client
 
     logger.info("Got connectorcreate request: %s", body)
     crd = TwingateConnectorCRD(**body)
+    connector_id = crd.spec.id
 
     if not crd.spec.id:
         connector = client.connector_create(
             crd.spec.name, memo.twingate_settings.remote_network_id
         )
+        connector_id = connector.id
         patch.spec["id"] = connector.id
         patch.spec["name"] = connector.name
     else:
@@ -78,56 +86,35 @@ def twingate_connector_create(body, spec, memo, logger, namespace, patch, **_):
 
     logger.info("connector: %s", connector)
     tokens = client.connector_generate_tokens(connector.id)
+    image_tag = crd.spec.get_image_tag_by_policy()
 
-    pod = get_connector_pod(crd.metadata.name, settings.full_url, crd.spec)
+    pod = get_connector_pod(crd, settings.full_url, image_tag)
     secret = get_connector_secret(tokens.access_token, tokens.refresh_token)
     kopf.adopt([pod, secret], owner=body, strict=True, forced=True)
-    kopf.label([pod, secret], {"twingate.com/owner": "connector"})
+    kopf.label([pod, secret], {"twingate.com/connector": crd.metadata.name})
 
     kapi = kubernetes.client.CoreV1Api()
     kapi.create_namespaced_secret(namespace=namespace, body=secret)
     kapi.create_namespaced_pod(namespace=namespace, body=pod)
 
+    return success(twingate_id=connector_id, image_tag=image_tag)
 
-@kopf.on.delete("", "v1", "pods", labels={"twingate.com/owner": "connector"})
-def twingate_connector_pod_deleted(body, spec, meta, logger, namespace, memo, **_):
-    logger.info("twingate_connector_pod_deleted: %s", body)
 
-    connector_owner = next(
-        (o for o in meta["ownerReferences"] if o["kind"] == "TwingateConnector"), None
-    )
-    if not connector_owner:
-        return None
+@kopf.on.update(
+    "twingateconnector", labels={"twingate.com/connector-pod-deleted": "true"}
+)
+def twingate_connector_recreate_pod(body, namespace, memo, logger, **_):
+    logger.info("twingate_connector_recreate_pod: %s.", body)
+    settings = memo.twingate_settings
+    crd = TwingateConnectorCRD(**body)
+    image_tag = crd.spec.get_image_tag_by_policy()
 
-    owner_group, owner_version = connector_owner["apiVersion"].split("/")
+    pod = get_connector_pod(crd, settings.full_url, image_tag)
+    kopf.adopt(pod, owner=body, strict=True, forced=True)
+    kopf.label(pod, {"twingate.com/connector": crd.metadata.name})
 
-    try:
-        kapi = kubernetes.client.CustomObjectsApi()
-        response = kapi.get_namespaced_custom_object(
-            owner_group,
-            owner_version,
-            namespace,
-            "twingateconnectors",
-            connector_owner["name"],
-        )
-        connector = TwingateConnectorCRD(**response)
-        pod = get_connector_pod(memo.twingate_settings.full_url, connector.spec)
-        kopf.adopt(pod, owner=response)
-        kopf.label(pod, {"operator.twingate.com/owner": "connector"})
-
-        # logger.info("pod!: %s", pod)
-        kapi = kubernetes.client.CoreV1Api()
-        result = kapi.create_namespaced_pod(namespace=namespace, body=pod)
-        logger.info("result: %s", result)
-    except kubernetes.client.exceptions.ApiException as api_ex:
-        if api_ex.status == 404:
-            logger.warning(
-                "ResourceAccessCRD.get_resource_ref_object: resource not found."
-            )
-        else:
-            logger.exception("ResourceAccessCRD.get_resource_ref_object failed")
-
-        return None
+    kapi = kubernetes.client.CoreV1Api()
+    kapi.create_namespaced_pod(namespace=namespace, body=pod)
 
 
 @kopf.on.delete("twingateconnector")
@@ -139,3 +126,33 @@ def twingate_connector_delete(spec, status, memo, logger, **kwargs):
     if connector_id := spec.get("id"):
         logger.info("Deleting connector %s", connector_id)
         memo.twingate_client.connector_delete(connector_id)
+
+
+@kopf.on.delete("", "v1", "pods", labels={"twingate.com/connector": kopf.PRESENT})
+def twingate_connector_pod_deleted(body, spec, meta, logger, namespace, memo, **_):
+    logger.info("twingate_connector_pod_deleted: %s", body)
+
+    # Annotate the parent connector so it knows it needs to recreate the pod
+    owner_refs = meta.get("ownerReferences", [])
+    owner = next((o for o in owner_refs if o["kind"] == "TwingateConnector"), None)
+    if not owner:
+        return
+
+    owner_group, owner_version = owner["apiVersion"].split("/")
+
+    try:
+        kapi = kubernetes.client.CustomObjectsApi()
+        kapi.patch_namespaced_custom_object(
+            owner_group,
+            owner_version,
+            namespace,
+            "twingateconnectors",
+            owner["name"],
+            {"metadata": {"labels": {"twingate.com/connector-pod-deleted": "true"}}},
+        )
+    except kubernetes.client.exceptions.ApiException as ex:
+        if ex.reason == "Conflict":
+            # This would happen when TwingateConnector is deleted -> pods is deleted -> tries to update its owner
+            # We can ignore this
+            return
+        logger.exception("Failed to annotate connector %s", owner["name"])

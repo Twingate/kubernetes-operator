25d40541f305c52fbdcc1bc1af1c7c9fb8dfb9b7
Eran Kampf <eran@ekampf.com>
2023-11-13 15:29:32 -0800

    test: remove unused fixture

--- app/tests/test_crds.py
+++ app/tests/test_crds.py
@@ -1,16 +1,4 @@
-from unittest.mock import patch
-
-import pytest
-
 from app.crds import K8sMetadata
-
-
-@pytest.fixture()
-def mock_get_namespaced_custom_object():
-    with patch(
-        "kubernetes.client.CustomObjectsApi.get_namespaced_custom_object"
-    ) as mock:
-        yield mock
 
 
 def test_k8smetadata_owner_reference_object():

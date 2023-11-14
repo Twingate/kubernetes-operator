bdb85e3cd1e742f2c21a6fd84d2706321deb56af
Eran Kampf <eran@ekampf.com>
2023-11-13 20:26:24 -0800

    define constructor in base

--- app/version_policy_providers/base.py
+++ app/version_policy_providers/base.py
@@ -5,6 +5,10 @@ from semantic_version import Version
 
 class VersionPolicyProvider:
     @abstractmethod
+    def __init__(self, repository: str | None = None):
+        ...
+
+    @abstractmethod
     def get_latest(
         self, specifier: str, *, allow_prerelease: bool = False
     ) -> Version | None:

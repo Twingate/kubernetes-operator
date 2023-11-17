6f8e42bacfde29d08b1862f64f3021dcd1416a26
Eran Kampf <eran@ekampf.com>
2023-11-17 14:41:46 -0800

    try

--- scripts/minikube-smoketests.sh
+++ scripts/minikube-smoketests.sh
@@ -26,7 +26,7 @@ fi
 
 # util to install a released minikube version into ${BIN_DIR}
 install_minikube_release() {
-	MINIKUBE_BINARY_URL="https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
+	MINIKUBE_BINARY_URL="https://storage.googleapis.com/minikube/releases/1.27.4/minikube-linux-amd64"
 	wget -nv -O "${MINIKUBE}" "${MINIKUBE_BINARY_URL}"
 	chmod +x "${MINIKUBE}"
 }
@@ -36,7 +36,7 @@ main() {
 	install_minikube_release
 
 	# create a cluster
-	"${MINIKUBE}" start --force --extra-config=apiserver.authorization-mode=AlwaysAllow
+	"${MINIKUBE}" start --force --kubernetes-version=1.27.4
 	"${MINIKUBE}" kubectl -- get pods -A
 
 	"${MINIKUBE}" kubectl -- apply -f ./deploy/twingate-operator/crds/ || true

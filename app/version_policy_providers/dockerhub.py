4c8538f26067780786cbf36ead131d18444bb97d
Eran Kampf <eran@ekampf.com>
2023-11-28 08:40:08 -0800

    paginate

--- app/version_policy_providers/dockerhub.py
+++ app/version_policy_providers/dockerhub.py
@@ -15,15 +15,24 @@ class DockerhubVersionPolicyProvider(VersionPolicyProvider):
             f"{self._DOCKER_HUB_API_BASE_URL}/repositories/{self.repository}/tags"
         )
 
-    def __call(self):
-        url = f"{self.tags_api_url}?page_size=100"
+    def __call(self, page: int = 0):
+        url = f"{self.tags_api_url}?page_size=100&page={page}"
         response = requests.get(url, timeout=6)
         return response.json()
 
+    def __get_all_api_pages(self):
+        page = 0
+        while True:
+            data = self.__call(page)
+            yield data
+            if not data["next"]:
+                break
+            page += 1
+
     def get_all_tags(self) -> Iterator[str]:
-        data = self.__call()
-        for result in data["results"]:
-            yield result["name"]
+        for data in self.__get_all_api_pages():
+            for result in data["results"]:
+                yield result["name"]
 
     def get_all_semver_tags(
         self, *, allow_prerelease: bool = False

f69a9167fc49e16611f37751f50c7199052e0e26
Eran Kampf <eran@ekampf.com>
2024-03-01 08:39:10 -0800

    fix name

--- tests_integration/test_connector_flows.py
+++ tests_integration/test_connector_flows.py
@@ -151,7 +151,9 @@ def test_connector_flows_image_change(kopf_settings, kopf_runner_args, ci_run_nu
     assert runner.exit_code == 0
 
 
-def test_pod_gone_while_operator_down(kopf_settings, kopf_runner_args, ci_run_number):
+def test_connector_flows_pod_gone_while_operator_down(
+    kopf_settings, kopf_runner_args, ci_run_number
+):
     connector_name = f"test-connector-gone-{ci_run_number}"
     OBJ = f"""
         apiVersion: twingate.com/v1beta

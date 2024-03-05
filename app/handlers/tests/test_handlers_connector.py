0ea650d673a277a2e5b4393f2cd7acbeac71ec92
Eran Kampf <eran@ekampf.com>
2024-03-05 12:44:37 -0800

    test_k8s_read_namespaced_reraises_non_404_exceptions

--- app/handlers/tests/test_handlers_connector.py
+++ app/handlers/tests/test_handlers_connector.py
@@ -70,6 +70,14 @@ def test_k8s_read_namespaced_pod_handles_404_returns_none(k8s_client_mock):
     assert k8s_read_namespaced_pod("default", "test") is None
 
 
+def test_k8s_read_namespaced_reraises_non_404_exceptions(k8s_client_mock):
+    k8s_client_mock.read_namespaced_pod.side_effect = (
+        kubernetes.client.exceptions.ApiException(status=500)
+    )
+    with pytest.raises(kubernetes.client.exceptions.ApiException):
+        k8s_read_namespaced_pod("default", "test")
+
+
 def test_twingate_connector_create(
     get_connector_and_crd, kopf_handler_runner, mock_api_client
 ):

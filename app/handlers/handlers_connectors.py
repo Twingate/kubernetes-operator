3fa9537b5bf24aff9b0881d1d7c54142f33f6d5c
Eran Kampf <eran@ekampf.com>
2024-03-11 14:06:38 -0700

    Fixed statement

--- app/handlers/handlers_connectors.py
+++ app/handlers/handlers_connectors.py
@@ -153,7 +153,8 @@ def twingate_connector_update(body, memo, logger, new, diff, status, namespace, 
     client = TwingateAPIClient(settings)
 
     crd = TwingateConnectorCRD(**body)
-    if len(diff) == 1 and diff[0][:3] == ("add", ("id"), None):
+    # (('add', ('id',), None, 'Q29ubmVjdG9yOjUwNjE3NQ=='),)
+    if len(diff) == 1 and diff[0][:3] == ("add", ("id",), None):
         return success(twingate_id=crd.spec.id, message="No update required")
 
     if not crd.spec.id:

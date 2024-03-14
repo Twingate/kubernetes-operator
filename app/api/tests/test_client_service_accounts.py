34a47a15ade75e0316e2d23805c04012fbd3e91c
Eran Kampf <eran@ekampf.com>
2024-03-14 15:36:21 -0700

    test_get_service_account_id_not_found_returns_none

--- app/api/tests/test_client_service_accounts.py
+++ app/api/tests/test_client_service_accounts.py
@@ -29,3 +29,21 @@ class TestTwingateServiceAccountAPIs:
         )
         result = api_client.get_service_account_id("test")
         assert result == "test-id"
+
+    def test_get_service_account_id_not_found_returns_none(
+        self, test_url, api_client, mocked_responses
+    ):
+        success_response = json.dumps({"data": {"serviceAccounts": {"edges": []}}})
+
+        mocked_responses.post(
+            test_url,
+            status=200,
+            body=success_response,
+            match=[
+                responses.matchers.json_params_matcher(
+                    {"variables": {"name": "test"}}, strict_match=False
+                )
+            ],
+        )
+        result = api_client.get_service_account_id("test")
+        assert result is None

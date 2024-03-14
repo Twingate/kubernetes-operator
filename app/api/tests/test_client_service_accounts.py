e9efbfa350fc906d37e8e0ca5261fd2a158820e3
Eran Kampf <eran@ekampf.com>
2024-03-14 15:34:40 -0700

    test_get_service_account_id_success

---
+++ app/api/tests/test_client_service_accounts.py
@@ -0,0 +1,31 @@
+import json
+
+import responses
+
+
+class TestTwingateServiceAccountAPIs:
+    def test_get_service_account_id_success(
+        self, test_url, api_client, mocked_responses
+    ):
+        success_response = json.dumps(
+            {
+                "data": {
+                    "serviceAccounts": {
+                        "edges": [{"node": {"id": "test-id", "name": "test"}}]
+                    }
+                }
+            }
+        )
+
+        mocked_responses.post(
+            test_url,
+            status=200,
+            body=success_response,
+            match=[
+                responses.matchers.json_params_matcher(
+                    {"variables": {"name": "test"}}, strict_match=False
+                )
+            ],
+        )
+        result = api_client.get_service_account_id("test")
+        assert result == "test-id"

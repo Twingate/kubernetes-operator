14a6043b94f6ddf8b5ed3688a046bc706074d74f
Eran Kampf <eran@ekampf.com>
2025-03-21 11:38:38 -0700

    add test

--- app/api/tests/test_client_resource_access.py
+++ app/api/tests/test_client_resource_access.py
@@ -35,6 +35,34 @@ class TestResourceAccessAPIs:
             security_policy_id=security_policy_id,
         )
 
+    def test_resource_access_add_success_without_policy(
+        self, test_url, api_client, mocked_responses
+    ):
+        resource_id = "test-resource"
+        principal_id = "test-principal"
+
+        expected_variables = {
+            "resourceId": resource_id,
+            "access": [{"principalId": principal_id, "securityPolicyId": None}],
+        }
+
+        mocked_responses.post(
+            test_url,
+            status=200,
+            body=json.dumps({"data": {"resourceAccessAdd": {"ok": True}}}),
+            match=[
+                responses.matchers.json_params_matcher(
+                    {"variables": expected_variables}, strict_match=False
+                )
+            ],
+        )
+
+        assert api_client.resource_access_add(
+            resource_id=resource_id,
+            principal_id=principal_id,
+            security_policy_id=None,
+        )
+
     def test_resource_access_add_failure(self, test_url, api_client, mocked_responses):
         resource_id = "test-resource"
         principal_id = "test-principal"

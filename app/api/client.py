939a33ed17572dcd9474ca61a364c51ed0e7820e
Eran Kampf <eran@ekampf.com>
2025-05-06 15:25:24 -0700

    fix request logging

--- app/api/client.py
+++ app/api/client.py
@@ -5,7 +5,7 @@ import requests
 from gql import Client
 from gql.transport.exceptions import TransportAlreadyConnected
 from gql.transport.requests import RequestsHTTPTransport
-from graphql import DocumentNode
+from graphql import DocumentNode, print_ast
 from requests.adapters import HTTPAdapter, Retry
 
 from app import typedefs
@@ -106,7 +106,7 @@ class TwingateAPIClient(
     def execute_gql(
         self, document: DocumentNode, variable_values: dict[str, Any] | None = None
     ):
-        self.logger.info("Calling %s with %s", document, variable_values)
+        self.logger.info("Calling %s with %s", print_ast(document), variable_values)
         result = self.client.execute(document, variable_values=variable_values)
         self.logger.info("Result: %s", result)
         return result

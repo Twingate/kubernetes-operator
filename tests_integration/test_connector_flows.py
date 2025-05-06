8f545edd8de2a309f02a0559f05a43c1ace45f12
Eran Kampf <eran@ekampf.com>
2025-05-06 16:00:17 -0700

    int tests

--- tests_integration/test_connector_flows.py
+++ tests_integration/test_connector_flows.py
@@ -3,14 +3,11 @@ import time
 from unittest.mock import ANY
 
 from tests_integration.utils import (
-    kubectl,
     kubectl_create,
     kubectl_delete,
-    kubectl_delete_wait,
     kubectl_get,
     kubectl_patch,
     kubectl_wait_deployment_available,
-    kubectl_wait_pod_running,
     kubectl_wait_to_exist,
 )
 
@@ -32,14 +29,13 @@ def test_connector_flows(run_kopf, random_name_generator):
                 repository: twingate/connector
                 version: "^1.0.0"
     """
+    wait_for_deployment = functools.partial(
+        kubectl_wait_deployment_available,
+        connector_name,
+    )
 
     with run_kopf():
         kubectl_create(OBJ)
-
-        wait_for_deployment = functools.partial(
-            kubectl_wait_deployment_available,
-            connector_name,
-        )
 
         secret = kubectl_wait_to_exist("secret", connector_name)
         deployment = wait_for_deployment()
@@ -74,12 +70,6 @@ def test_connector_flows(run_kopf, random_name_generator):
         assert pod["spec"]["containers"][0]["env"][-1]["value"] == "bar"
         assert pod["metadata"]["annotations"]["some/annotation"] == "some-value"
 
-        kubectl_delete_wait("tc", connector_name)
-
-        # secret & pod are deleted
-        kubectl_delete_wait("secret", connector_name, perform_deletion=False)
-        kubectl_delete_wait("pod", connector_name, perform_deletion=False)
-
 
 def test_connector_flows_image_change(run_kopf, random_name_generator):
     connector_name = random_name_generator("t-image")
@@ -92,13 +82,19 @@ def test_connector_flows_image_change(run_kopf, random_name_generator):
             name: {connector_name}
             hasStatusNotificationsEnabled: false
             image:
-                tag: "1.62.0"
+                tag: "1.74.0"
     """
+    wait_for_deployment = functools.partial(
+        kubectl_wait_deployment_available,
+        connector_name,
+    )
 
     with run_kopf():
         kubectl_create(OBJ)
+
         secret = kubectl_wait_to_exist("secret", connector_name)
-        pod = kubectl_wait_pod_running(connector_name)
+        wait_for_deployment()
+
         connector = kubectl_get("tc", connector_name)
 
         # connector was properly provisioned
@@ -107,27 +103,17 @@ def test_connector_flows_image_change(run_kopf, random_name_generator):
 
         # Secret was properly created
         assert secret["data"] == {"TWINGATE_ACCESS_TOKEN": ANY, "TWINGATE_REFRESH_TOKEN": ANY}  # fmt: skip
-        # pod was properly created
-        assert pod["status"]["phase"] == "Running"
-        assert pod["metadata"]["ownerReferences"][0]["name"] == connector_name
-        assert pod["metadata"]["ownerReferences"][0]["kind"] == "TwingateConnector"
 
         # Change image tag
         # kubectl patch tc/test-connector-image-local -p '{"spec": {"image": {"tag": "1.63.0"}}}' --type=merge
-        kubectl_patch(f"tc/{connector_name}", {"spec": {"image": {"tag": "1.63.0"}}})
+        kubectl_patch(f"tc/{connector_name}", {"spec": {"image": {"tag": "1.75.0"}}})
         time.sleep(5)
-        pod = kubectl_wait_pod_running(connector_name)
-        assert pod["status"]["phase"] == "Running"
-        assert pod["spec"]["containers"][0]["image"] == "twingate/connector:1.63.0"
-
-        kubectl_delete_wait("tc", connector_name)
-
-        # secret & pod are deleted
-        kubectl_delete_wait("secret", connector_name, perform_deletion=False)
-        kubectl_delete_wait("pod", connector_name, perform_deletion=False)
+        wait_for_deployment()
 
 
-def test_connector_flows_pod_gone_while_operator_down(run_kopf, random_name_generator):
+def test_connector_flows_deployment_gone_while_operator_down(
+    run_kopf, random_name_generator
+):
     connector_name = random_name_generator("t-gone")
     OBJ = f"""
         apiVersion: twingate.com/v1beta
@@ -141,76 +127,24 @@ def test_connector_flows_pod_gone_while_operator_down(run_kopf, random_name_gene
                 tag: "1.63.0"
     """
 
-    with run_kopf():
+    wait_for_deployment = functools.partial(
+        kubectl_wait_deployment_available,
+        connector_name,
+    )
+
+    with run_kopf(cleanup=False):
         kubectl_create(OBJ)
         kubectl_wait_to_exist("secret", connector_name)
-        kubectl_wait_pod_running(connector_name)
+        wait_for_deployment()
         connector = kubectl_get("tc", connector_name)
 
         # connector was properly provisioned
         expected_status = {"success": True, "ts": ANY, "twingate_id": ANY}
         assert connector["status"]["twingate_connector_create"] == expected_status
 
-    kubectl_delete("pod", connector_name)
+    kubectl_delete("deployment", connector_name)
 
     # run operator again
     with run_kopf():
-        # pod was recreated
-        pod = kubectl_wait_pod_running(connector_name)
-        assert pod["status"]["phase"] == "Running"
-
-        # Test done, delete connector
-        kubectl_delete("tc", connector_name)
-        time.sleep(5)
-
-
-def test_connector_flows_pod_migration_from_older_pod_with_finalizers(
-    run_kopf, ci_run_number, random_name_generator
-):
-    connector_name = random_name_generator("t-migration")
-    OBJ = f"""
-        apiVersion: twingate.com/v1beta
-        kind: TwingateConnector
-        metadata:
-            name: {connector_name}
-        spec:
-            name: {connector_name}
-            hasStatusNotificationsEnabled: false
-            image:
-                tag: "1.63.0"
-    """
-
-    with run_kopf():
-        kubectl_create(OBJ)
-        kubectl_wait_to_exist("secret", connector_name)
-        kubectl_wait_pod_running(connector_name)
-        connector = kubectl_get("tc", connector_name)
-
-        # connector was properly provisioned
-        expected_status = {"success": True, "ts": ANY, "twingate_id": ANY}
-        assert connector["status"]["twingate_connector_create"] == expected_status
-
-        # Patch pod to add finalizers and make operator think its old
-        kubectl_patch(
-            f"pod/{connector_name}",
-            {
-                "metadata": {
-                    "finalizers": ["twingate.com/finalizer"],
-                }
-            },
-        )
-        kubectl(
-            f"annotate pod/{connector_name} --overwrite twingate.com/connector-podspec-version=not_what_op_expects"
-        )
-
-        # wait for timer to run
-        time.sleep(10)
-
-        # check pod was recreated
-        pod = kubectl_wait_pod_running(connector_name)
-        assert pod["metadata"]["annotations"]["twingate.com/connector-podspec-version"] == "v1"  # fmt: skip
-        assert pod["status"]["phase"] == "Running"
-
-        # Test done, delete connector
-        kubectl_delete("tc", connector_name)
-        time.sleep(5)
+        # deployment recreated was recreated
+        wait_for_deployment()
